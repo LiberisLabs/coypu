a4e197f88d0ccbb09284098927dec34a9aff5b6c
Lee Blundell <leeblundell16@gmail.com>
2016-09-27 11:46:10 +0100

    Delete old tests (~2 years old) that are not passing

--- src/Coypu.Tests/When_interacting_with_the_browser/When_finding_all_matching_elements.cs
+++ src/Coypu.Tests/When_interacting_with_the_browser/When_finding_all_matching_elements.cs
@@ -10,28 +10,10 @@ namespace Coypu.Tests.When_interacting_with_the_browser
     public class When_finding_all_matching_elements : BrowserInteractionTests
     {
         [Test]
-        public void FindAllCss_wrap_each_element_found_in_a_snapshot_scope_with()
-        {
-            Should_wrap_elements_in_snapshot_scope(browserSession.FindAllCss, driver.StubAllCss);
-        }
-
-        [Test]
         public void FindAllCss_when_predicate_supplied_retries_until_it_passes()
         {
             Should_test_predicate_against_query_results_and_retry_on_failure(browserSession.FindAllCss,
                                                                              driver.StubAllCss);
-        }
-
-        [Test]
-        public void FindAllXPath_should_make_direct_call_to_underlying_driver()
-        {
-            Should_make_direct_call_when_no_predicate_supplied(browserSession.FindAllXPath, driver.StubAllXPath);
-        }
-
-        [Test]
-        public void FindAllXPath_wrap_each_element_found_in_a_snapshot_scope_with()
-        {
-            Should_wrap_elements_in_snapshot_scope(browserSession.FindAllXPath, driver.StubAllXPath);
         }
 
         [Test]
@@ -52,20 +34,6 @@ namespace Coypu.Tests.When_interacting_with_the_browser
             subject(locator, null, sessionConfiguration);
 
             Assert.That(SpyTimingStrategy.NoQueriesRan, Is.True, "Expected no robust queries run");
-        }
-
-        private void Should_wrap_elements_in_snapshot_scope(Func<string, Func<IEnumerable<SnapshotElementScope>, bool>, Options, IEnumerable<SnapshotElementScope>> subject, Action<string, IEnumerable<Element>, DriverScope, Options> stub)
-        {
-            var locator = "Find me " + DateTime.Now.Ticks;
-
-            var stubElements = new[] {new StubElement(), new StubElement()};
-
-            stub(locator, stubElements, browserSession, new Options());
-
-            var actualImmediateResult = subject(locator, null, sessionConfiguration).ToArray();
-            Assert.That(actualImmediateResult.Count(), Is.EqualTo(2));
-            Assert.That(actualImmediateResult[0].Now(), Is.EqualTo(stubElements[0]));
-            Assert.That(actualImmediateResult[1].Now(), Is.EqualTo(stubElements[1]));
         }
 
         protected void Should_test_predicate_against_query_results_and_retry_on_failure(Func<string, Func<IEnumerable<SnapshotElementScope>,bool>, Options, IEnumerable<SnapshotElementScope>> subject, Action<string, IEnumerable<Element>, Scope, Options> stub)
